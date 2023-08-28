9f7218a0c518c913a03cf2966c68b13a234981f8
M0mahmoud <houdmohamed85@gmail.com>
2023-08-28 23:04:17 +0300

    Jobs Routes

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,18 +1,22 @@
 import { Router } from "express";
 
 import {
+  deleteJob,
   getAllJobs,
   getOneJob,
   postNewJob,
+  updateJob,
 } from "../controller/jobsController.js";
 import isAuthenticated from "../middleware/isAuth.js";
 const router = Router();
 
 // Puplic Routes
-router.get("/jobs", getAllJobs);
+router.get("/jobs/:page", getAllJobs);
 router.get("/job/:jobId", getOneJob);
 
 // Private Routes
 router.post("/post-job", isAuthenticated, postNewJob);
+router.put("/update-job/:jobId", isAuthenticated, updateJob);
+router.delete("/delete-job/:jobId", isAuthenticated, deleteJob);
 
 export default router;
