74d4a1b1a40c30cd92a6b9c1da11e74fa4c759be
M0mahmoud <houdmohamed85@gmail.com>
2023-08-28 23:05:42 +0300

    Revert "Jobs Routes"
    This reverts commit 9f7218a0c518c913a03cf2966c68b13a234981f8.

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,22 +1,18 @@
 import { Router } from "express";
 
 import {
-  deleteJob,
   getAllJobs,
   getOneJob,
   postNewJob,
-  updateJob,
 } from "../controller/jobsController.js";
 import isAuthenticated from "../middleware/isAuth.js";
 const router = Router();
 
 // Puplic Routes
-router.get("/jobs/:page", getAllJobs);
+router.get("/jobs", getAllJobs);
 router.get("/job/:jobId", getOneJob);
 
 // Private Routes
 router.post("/post-job", isAuthenticated, postNewJob);
-router.put("/update-job/:jobId", isAuthenticated, updateJob);
-router.delete("/delete-job/:jobId", isAuthenticated, deleteJob);
 
 export default router;
